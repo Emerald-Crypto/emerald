a1d02eb9167eeaacbc480d3191500c42a6b586c8
emerald <emerald@gmail.com>
2013-06-25 16:00:37 -0700

    modified:   src/checkpoints.cpp 	modified:   src/qt/res/icons/bitcoin.ico 	modified:   src/qt/res/icons/bitcoin.png 	modified:   src/qt/res/icons/bitcoin_testnet.png

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,27 +27,14 @@ namespace Checkpoints
 	// no checkpoint now, can be added in later releases
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
-            (  1, uint256("0x01aa92d97daf7fa2987ffb0ec5778f35cdaf8cb2948018e636482693b43ff9fd"))
-            ( 51, uint256("0x83ba6777c3527ee8996fa568ca214df7860405e9078f3b974adc540770287882"))
-            (131, uint256("0x7f0effa98470562fd5b31f8c539ce2ee89efde6a8282a11854001c67a480da33"))
-            (207, uint256("0xda14d64c6fbf20d7c78cb6f74aa578217b61097c7e16301f38af465ca7b1286d"))
+            (    1, uint256("0x01aa92d97daf7fa2987ffb0ec5778f35cdaf8cb2948018e636482693b43ff9fd"))
+            (   51, uint256("0x83ba6777c3527ee8996fa568ca214df7860405e9078f3b974adc540770287882"))
+            (  131, uint256("0x7f0effa98470562fd5b31f8c539ce2ee89efde6a8282a11854001c67a480da33"))
+            (  207, uint256("0xda14d64c6fbf20d7c78cb6f74aa578217b61097c7e16301f38af465ca7b1286d"))
+            (30561, uint256("0x42ac3fde352cc2f3be3fe2b720586affdecae00b601d1de4f83da0c5a31ff285"))
+            (38967, uint256("0x794fdb36a62bbe9ec6feb4bd53e7c474be00b5ff4d617454f74716f8b40813d2"))
 			;
-        /*
-        boost::assign::map_list_of
-        (     1, uint256("0x000000552acd41b1f9ae21fc7eb4054dce1331c8755f37f3d30a15825f695cd4"))
-        (     2, uint256("0x000008559730ac71244fcca965c73077616a3cebff5e28dadeb61e8ddae631ae"))
-        (  1500, uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967"))
-        (  4032, uint256("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
-        (  8064, uint256("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
-        ( 16128, uint256("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
-        ( 23420, uint256("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507"))
-        ( 50000, uint256("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6"))
-        ( 80000, uint256("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a"))
-        (120000, uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131"))
-        (161500, uint256("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43"))
-        (179620, uint256("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709"))
-        ;
-        */
+
 
     bool CheckBlock(int nHeight, const uint256& hash)
     {
